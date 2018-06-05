4da06e4fb2ed001fdf24f5e933d13ef4c0c2d1c1
Jason Schmitt <schmitty890@gmail.com>
2018-06-05 16:20:27 -0400

    adding gulp npm start script

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,6 +1,6 @@
 //the Test Module is a namespaced function to what feature of the site this function powers
 var Test = function(){
-  var aTestVariable = 'test!!';
+  var aTestVariable = 'test';
 
   //another function private to Module
   function privateFunction(){
