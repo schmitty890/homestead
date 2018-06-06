3fb549d2715b3d8080ea9f046f5ab2d3eb596188
Jason Schmitt <schmitty890@gmail.com>
2018-06-06 01:10:52 -0400

    header handlebar partial

--- public/assets/build/js/app.js
+++ public/assets/build/js/app.js
@@ -5,7 +5,10 @@ var Test = function(){
   //another function private to Module
   function privateFunction(){
     //this can log the 'aTestVariable' because it is defined outside of this Module function, but aTestVariable is kept private to this Module
-    console.log(aTestVariable);
+    if (aTestVariable === 'test') {
+      console.log(aTestVariable);
+    }
+
   }
 
   //this init function initializes the functions ran
