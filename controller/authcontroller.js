ed41a74a686281064bf0ecc2a4d96fa9e3279606
Joe Wright <joewrightart@gmail.com>
2018-06-06 22:44:31 -0400

    lint rolled

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,24 +1,25 @@
-var exports = module.exports = {}
-var db = require("../models");
+const exports = module.exports = {}
+//not using this yet
+//const db = require("../models");
 
 exports.signup = function(req,res){
-	res.render('signup'); 
+  res.render('signup'); 
 }
 
 exports.signin = function(req,res){
-	res.render('signin'); 
+  res.render('signin'); 
 }
 
 //example of how we can grab data coming through the route before rendering.
 //when in doubt about carrying data into a new route, console.log your req here.
 //hit me up if you run into confusion obviously -Joe
-exports.account = function (req, res) {
+exports.account = function(req, res) {
   res.render('account', { userId: req.user.id });
 }
 
 // logout route from header lands here and destroys the express session then redirects to home.
 exports.logout = function(req,res){
-  req.session.destroy(function(err) {
-  res.redirect('/');
+  req.session.destroy(function() {
+    res.redirect('/');
   });
 }
