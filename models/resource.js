14aacba19f054a186dd7c1e95363b8844f718308
unknown <tommyn.gre@gmail.com>
2018-06-08 01:54:19 -0400

    + resource model, call seeds.js in server.js

---
+++ models/resource.js
@@ -0,0 +1,65 @@
+module.exports = function(sequelize, Sequelize) {
+  const Resource = sequelize.define('resource', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    author_id: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+    //will try to implement via
+    ///association with user
+    ///storing these (again)
+    ///seems suboptimal
+    ///
+    // author_name: {
+    //   type: Sequelize.STRING,
+    //   notEmpty: true
+    // },
+    // username: {
+    //   type: Sequelize.TEXT
+    // },
+    // email: {
+    //   type: Sequelize.STRING,
+    //   validate: {
+    //     isEmail: true
+    //   }
+    // },
+    title: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      validate: {
+        len: [1,50]
+      }
+    },
+    description: {
+      type: Sequelize.TEXT,
+      notEmpty: true,
+      validate: {
+        len: [1,1000]
+      }
+    },    
+    price: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+      validate: {
+        isInt: true
+      }
+    },
+    category: {
+      type: Sequelize.ENUM('service needed','service offered','item needed','item offered'),
+      notEmpty: true
+    },
+    //with the wide variety
+    //of services/items we'll have
+    //ENUM seems too cumbersome
+    //STRING for now
+    condition: {
+      type: Sequelize.STRING,
+      defaultValue: null
+    }
+  });
+  return Resource;
+}
