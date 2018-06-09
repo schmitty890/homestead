4eb8ae22c7678135f282179eb8b5b880005c7dee
Jason Schmitt <schmitty890@gmail.com>
2018-06-08 23:05:42 -0400

    typo to events

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,8 +22,8 @@ module.exports = function(app) {
   app.post("/api/events", function(req, res) {
     var userId = '';
     db.event.create(req.body)
-      .then(function(classified) {
-        res.json(classified);
+      .then(function(events) {
+        res.json(events);
       })
       .catch(function(err) {
         res.json({ status: "ERROR", message: err })
