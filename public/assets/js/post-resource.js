2a9515c9f03f11939e456097b451779fe512e9fb
unknown <tommyn.gre@gmail.com>
2018-06-10 12:42:33 -0400

    + post form & js

---
+++ public/assets/js/post-resource.js
@@ -0,0 +1,61 @@
+/**
+ * [PostResource captures values from the form on /postevent]
+ * steps
+ * 1. on submit of the postevent collect values from form
+ * 2. pass values of form to ajax call to the backend api, /api/resource
+ */
+var PostResource = function () {
+
+  $('#submit-category').selectize({
+    sortField: 'text'
+  });
+
+  var formValues = {};
+  $("#date").flatpickr({
+    enableTime: true,
+    dateFormat: "Y-m-d H:i"
+  });
+
+  function gatherFormElements() {
+    formValues = {
+      title: $('#title').val().trim(),
+      date: $('#date').val().trim(),
+      description: $('#details').val().trim()
+    }
+    if (formValues.title === '' || formValues.description === '') {
+      alert('fill out ALL required forms.');
+    }
+    passToBackend(formValues);
+  }
+
+  function passToBackend(formValues) {
+    console.log(formValues);
+    $.ajax('/api/resources', {
+      type: 'POST',
+      data: formValues
+    }).then(
+      function () {
+        //redirect to resource page?
+        location.reload();
+      }
+    );
+  }
+
+  function onSubmit() {
+    $(document).on('submit', '#post-resource-form', function (e) {
+      e.preventDefault();
+      gatherFormElements();
+    });
+  }
+
+  function init() {
+    onSubmit();
+  }
+
+  return {
+    init: init
+  }
+
+}();
+
+PostResource.init();
