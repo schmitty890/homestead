44351a73f18a4809b421a6fe6da2dac98a82a007
Jason Schmitt <schmitty890@gmail.com>
2018-06-14 23:48:42 -0400

    hotfix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -136,26 +136,45 @@ module.exports = function (app) {
     });
   }, function (req, res, next) {
     hbsObject = res.locals.user;
+    console.log(hbsObject);
     var categories = ['bikes', 'electronics', 'appliances', 'babyKid', 'clothes', 'furniture', 'lawn', 'music', 'sports', 'autos', 'phones', 'tickets'];
-    // var categories = ['clothes', 'appliances'];
     // loop over array of categories
     Promise.all(categories.map(category =>
       db.classifieds.findAll({
         where: {
           category: category
         },
+        limit: 1,
         order: [
           ['createdAt', 'DESC']
         ]
       })
       )).then(function (classifiedAd) {
-        // console.log('--------------------------------');
+        console.log('-----------start---------------------');
         console.log(classifiedAd);
+        classifiedAd.forEach(function(classified) {
+          console.log('-----------classy start---------------------');
+          // console.log(classy.length);
+          // hbsObject[] = {
 
+          // }
+          // console.log(classy);
+          classified.forEach(function(element) {
+            console.log(element.dataValues);
+            hbsObject[element.category] = {
+              latest: element.dataValues
+            }
+          });
+          // console.log(classy[0].classifieds.dataValues.username)
+          // console.log(classy)
+          console.log('-----------classy finished---------------------');
+        });
+        console.log('------------finish--------------------');
         // hbsObject[category] = {
         //   classifiedCount: classifiedAd.length,
         //   latest: classifiedAd[0]
         // }
+        res.render('classifieds', { hbsObject: hbsObject });
       });
 
 
@@ -179,7 +198,7 @@ module.exports = function (app) {
     //   });
     // });
 
-    res.render('classifieds', { hbsObject: hbsObject });
+
   });
 
   app.get('/classifieds/:category', function (req, res) {
