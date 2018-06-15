4d8873f36257d7d9179f5c582ad7d14088241a94
Joe Wright <joewrightart@gmail.com>
2018-06-15 14:54:53 -0400

    manager can now post dues from their portal, only messages portal left

---
+++ public/assets/js/post-dues.js
@@ -0,0 +1,23 @@
+$("#post-dues-form").on("submit", function (event) {
+  console.log("postduesreceived")
+  var newDues = {};
+  event.preventDefault();
+  if ($.trim($("#dues").val()) === "") {
+    alert('you did not fill out dues');
+  }
+  else {
+    newDues = {
+      dues: $("#dues").val().trim()
+    }
+    var updateEmail = $('#duesEmail').val().trim()
+
+    $.ajax("/api/postdues/"+updateEmail, {
+      type: "PUT",
+      data: newDues
+    }).then(
+      function() {
+        location.reload();
+      }
+    ); 
+  };
+});
